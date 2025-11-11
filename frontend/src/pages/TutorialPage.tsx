bf304c0fecd48ff90eb9c945b2556789e422b997
KIM SEONMIN <seonmiki@student.42seoul.kr>
2025-11-11 10:08:03 +0900

    [Feat] 튜토리얼 화면에 카메라 허용 안내 메시지 추가
    Former-commit-id: bac3cd5bc4d890f56478cf5150f19dc85dc38ebf

--- frontend/src/pages/TutorialPage.tsx
+++ frontend/src/pages/TutorialPage.tsx
@@ -5,7 +5,8 @@ function TutorialPage(){
   const nav = useNavigate();
   return <>
     <div className="tutorial-page">
-      <p> 버튼을 눌러 체조를 시작하세요! </p>
+      <p > 버튼을 눌러 체조를 시작하세요! </p>
+      <div className="info-msg"> ( 원할한 진행을 위해 카메라 허용을 미리 체크해주세요 ) </div>
       <button className="play-btn" onClick={() => nav("/game/test-song")}>
         <div className="play-icon" />
       </button>
