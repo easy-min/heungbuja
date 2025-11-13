605afe725de1957b2d66b82751af0cec9d8cb665
easy-min <minisolifewow@sookmyung.ac.kr>
2025-11-13 15:33:52 +0900

    :bug: [fix] segmentInfo 구조 정의 및 CommandSectionInfo.java - verse1cam, verse2cam 제거
    Former-commit-id: 45d758a1bd4c55697b5fc9c2e1cba23a3a134609

--- backend/spring-server/src/main/java/com/heungbuja/command/dto/CommandSectionInfo.java
+++ backend/spring-server/src/main/java/com/heungbuja/command/dto/CommandSectionInfo.java
@@ -14,17 +14,4 @@ public class CommandSectionInfo {
     private double verse1StartTime;
     private double breakStartTime;
     private double verse2StartTime;
-
-    private VerseInfo verse1cam;
-    private VerseInfo verse2cam;
-
-    /**
-     * 각 절(verse)의 시간 정보를 담는 내부 DTO
-     */
-    @Getter
-    @Builder
-    public static class VerseInfo {
-        private double startTime;
-        private double endTime;
-    }
 }
