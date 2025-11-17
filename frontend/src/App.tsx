2a9038459b03c187f07135c86c1072d8f079fc96
jayeunpark <jayeunpark0704@gmail.com>
2025-11-17 16:30:12 +0900

    [Chore] basename(user) 추가
    Former-commit-id: d113ea8303bc28823b504a911359315462ca103c

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -46,7 +46,7 @@ function App() {
         );
     }
   return (
-    <BrowserRouter>
+    <BrowserRouter basename='/user'>
       <div className="app">
         <Routes>
            {/* 로그인 페이지 - 환경에 따라 다른 컴포넌트 */}
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 export default defineConfig({
   plugins: [react()],
 
-  base: '/',
+  base: '/user/',
   resolve: {
   alias: {
       '@': path.resolve(__dirname, './src')
