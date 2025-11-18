1eeeb21ed1166410c1353d09f77fee39d936c013
KIM SEONMIN <seonmiki@student.42seoul.kr>
2025-11-18 22:38:19 +0900

    [Feat] 노래 목록 페이지 추가
    Former-commit-id: 1c7e1e85e6878aa053bdbcc1a500ecb9ea92f319

---
+++ frontend/src/pages/SongListPage.tsx
@@ -0,0 +1,11 @@
+import './SongListPage.css'
+import VoiceButton from '@/components/VoiceButton';
+
+function SongListPage(){
+  
+  return <>
+  <VoiceButton />
+  </>
+}
+
+export default SongListPage;
